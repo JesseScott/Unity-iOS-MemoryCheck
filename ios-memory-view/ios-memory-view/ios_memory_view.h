262ebf1767e82f3036d80d6c5656bf9ba45c53d0
Jesse Scott <jessecolinscott@gmail.com>
2016-02-12 11:31:16 -0800

    static library project

---
+++ ios-memory-view/ios-memory-view/ios_memory_view.h
@@ -0,0 +1,13 @@
+//
+//  ios_memory_view.h
+//  ios-memory-view
+//
+//  Created by Jesse Scott on 2016-02-12.
+//  Copyright © 2016 E-Link Entertainment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ios_memory_view : NSObject
+
+@end
---
+++ ios-memory-view/ios-memory-view/ios_memory_view.m
@@ -0,0 +1,13 @@
+//
+//  ios_memory_view.m
+//  ios-memory-view
+//
+//  Created by Jesse Scott on 2016-02-12.
+//  Copyright © 2016 E-Link Entertainment. All rights reserved.
+//
+
+#import "ios_memory_view.h"
+
+@implementation ios_memory_view
+
+@end
